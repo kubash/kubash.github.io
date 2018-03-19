3b319f4aa7cfe3cd09dfbb092e6c7285fc9132f7
Josh Cox <josh@webhosting.coop>
2018-03-18 23:07:45 -0500

    Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span>K8$</span>
+          <span>K8$</span>
         </div>
         <div className="content">
             <div className="inner">
